77f0adda99ca460da596b9db819371eefbd835c2
NISHNA <67039243+Nishna001@users.noreply.github.com>
2020-06-17 10:49:58 +0530

    Created project2.py where it displays positive numbers from a list.

---
+++ Project2.py
@@ -0,0 +1,9 @@
+lst = []
+n=int(input("Enter the number of elements: "),base=10)
+for i in range (0,n):
+    ele=int(input(),base=10)
+    lst.append(ele)
+
+for num in lst:
+    if num>=0:
+     print(num,end = " ")
